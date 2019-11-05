7da5421dcf5fd684da7bc547639f97dc66a32029
lilvina <davinaleo2007@yahoo.com>
2019-11-04 23:54:26 -0800

    solved maxchar, fizzbuzz and reverseint

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  //create an empty object to store our data
+  //loop over each character of the string
+  //if no character is found, set the character equal to 1
+  // otherwise, character will increase
+  const chars = {}
+  let max = 0
+  let maxCharacter = ''
+  for(let char of str) {
+    if(!chars[char]) {
+      chars[char] = 1
+    } else {
+      chars[char]++
+    }
+  }
+  for(let char in chars) {
+    if(chars[char] > max) {
+      max = chars[char]
+      maxCharacter = char
+    }
+  }
+  return maxCharacter
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,10 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  //use a toString method that will convert a number to a string
+  //then use the split, reverse and join method to reverse the number
+  return parseInt(n.toString().split('').reverse().join('')) * Math.sign(n)
+}
 
 module.exports = reverseInt;
