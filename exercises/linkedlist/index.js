93b0a29ab00701df44f07c77091f45bb1c06eb47
lilvina <davinaleo2007@yahoo.com>
2019-11-14 23:44:43 -0800

    finished more problems on linked lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -30,7 +30,36 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head 
+    return this.head
+  }
+
+  getLast() {
+    //if there is no head node, return nothing
+    if(!this.head) {
+      return null
+    }
+    //get reference to the current node
+    //while node exists, update node
+    //check to see if node.next does not exist, we must be at the end
+    let node = this.head
+    while(node) {
+      if(!node.next) {
+        return node
+      }
+      node = node.next
+    }
+  }
+
+  clear(){
+    return this.head = null
+  }
+
+  removeFirst() {
+    if(!this.head) {
+      return
+    }
+
+    this.head = this.head.next
   }
 }
 
