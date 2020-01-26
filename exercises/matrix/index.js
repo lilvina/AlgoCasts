b85b9db3d0b91b461d917dbd287cbddf3c6e3ddb
lilvina <davinaleo2007@yahoo.com>
2020-01-25 23:15:23 -0800

    worked on matrix problem

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,58 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  //create empty array of arrays called 'results'
+  // create a counter variable, starting at 1
+  // as long as (start column <= end column) and (start row <= end row)
+  // loop from start column to end column
+  // at results[start row][i] assign counter variable
+  //increment counter
+  // increment start row
+  // loop from start row to end row
+  // at results[i][end column] assign counter variable
+  // decrement end row
+  // repeat for other two sides
+  const results = []
+
+  for(let i = 0; i < n; i++){
+    results.push([])
+  }
+  let counter = 1
+  let startColumn = 0
+  let endColumn = n - 1
+  let startRow = 0
+  let endRow = n - 1
+
+  while(startColumn <= endColumn && startRow <= endRow) {
+    //top row
+    for(let i = startColumn; i <= endColumn; i++) {
+      results[startRow][i] = counter
+      counter++
+    }
+    startRow++
+    // right column
+    for(let i = startRow; i <= endRow; i++) {
+      results[i][endColumn] = counter
+      counter++
+    }
+    endColumn--
+
+    // bottom row
+    for(let i = endColumn; i >= startColumn; i--){
+      results[endRow][i] = counter
+      counter++
+    }
+    endRow--
+
+    //start column
+    for(let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = counter
+      counter++
+    }
+    startColumn++
+  }
+  return results
+}
 
 module.exports = matrix;
