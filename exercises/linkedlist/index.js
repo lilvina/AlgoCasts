355c8132b171969262b0fd03c061fa250efe6188
lilvina <davinaleo2007@yahoo.com>
2020-07-29 02:12:24 -0700

    finished the rest of linked lists

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -61,6 +61,72 @@ class LinkedList {
 
     this.head = this.head.next
   }
+  removeLast() {
+    if(!this.head) {
+      return null
+    }
+
+    if(!this.head.next) {
+      this.head = null
+      return
+    }
+
+    let prev = this.head
+    let node = this.head.next
+    while(node.next) {
+      prev = node
+      node = node.next
+    }
+    prev.next = null
+  }
+
+  insertLast(data) {
+    const last = this.getLast()
+    if(last) {
+      last.next = new Node(data)
+    } else {
+      this.head = new Node(data)
+    }
+  }
+  getAt(index) {
+    if(!this.head) {
+      return null
+    }
+    let counter = 0
+    let node = this.head
+    while(node) {
+      if(counter === index) {
+        return node
+      }
+      counter++
+      node = node.next
+    }
+    return null
+  }
+
+  removeAt() {
+    if(!this.head) {
+      return null
+    }
+
+    this.head = this.head.next
+  }
+
+  insertAt(data, index) {
+    if(!this.head) {
+      this.head = new Node(data)
+      return
+    }
+
+    if(index === 0) {
+      this.head = new Node(data, this.head)
+      return
+    }
+
+    const prev = this.getAt(index - 1) || this.getLast()
+    const node = new Node(data, prev.next)
+    prev.next = node
+  }
 }
 
 module.exports = { Node, LinkedList };
